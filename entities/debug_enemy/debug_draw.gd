ed45977f1e7b4b34f7e4f5c1d00e883c57bb6527
inconsistentlyMediocre <carlospcastro.ac@gmail.com>
2024-12-11 14:39:52 -0300

    Fixed steering behaviors on enemy

--- entities/debug_enemy/debug_draw.gd
+++ entities/debug_enemy/debug_draw.gd
@@ -4,6 +4,7 @@ var ray_directions: Array[Vector2] = []
 var rays_amount: int = 12
 var interest: Array[float] = []
 var danger: Array[float] = []
+var dir: Vector2 = Vector2.ZERO
 
 
 func _ready() -> void:
@@ -17,12 +18,15 @@ func _draw() -> void:
 		return
 	if interest.is_empty() or danger.is_empty():
 		return
+	if dir == Vector2.ZERO:
+		return
 	for i in rays_amount:
 		var angle: float = i * 2 * PI / rays_amount
 		ray_directions[i] = Vector2.RIGHT.rotated(angle)
 		var color: Color = Color(danger[i], interest[i], 0.0)
 		#color.v = 1.0
-		draw_line(position, ray_directions[i] * 16.0, color, 2.0)
+		draw_line(position, ray_directions[i] * 16.0, color, 1.0)
+	draw_line(position, position + dir * 64, Color.DARK_BLUE, 2.0)
 	#print("a")
 
 
--- entities/debug_enemy/enemy_movement_manager.gd
+++ entities/debug_enemy/enemy_movement_manager.gd
@@ -12,6 +12,12 @@ var target: Node2D
 var ray_directions: Array[Vector2] = []
 var interest: Array[float] = []
 var danger: Array[float] = []
+var has_line_of_sight: bool = false
+var last_known_position: Vector2 = Vector2.ZERO
+var target_position: Vector2 = Vector2.ZERO
+var target_reached: bool = false
+var direction: Vector2 = Vector2.ZERO
+
 var nav_agent_ready: bool = false
 
 
@@ -19,6 +25,7 @@ func _ready() -> void:
 	ray_directions.resize(rays_amount)
 	interest.resize(rays_amount)
 	danger.resize(rays_amount)
+	last_known_position = parent.global_position
 	if nav_agent:
 		nav_agent.target_desired_distance = look_ahead
 	for i in rays_amount:
@@ -35,85 +42,103 @@ func set_interest() -> void:
 			if nav_agent_ready:
 				desired_direction = parent.global_position.direction_to(nav_agent.get_next_path_position())
 		else:
-			desired_direction = parent.global_position.direction_to(target.global_position)
-		for i in rays_amount:
-			var interest_value: float = ray_directions[i].dot(desired_direction.normalized())
-			interest[i] = max(0.5, interest_value)
-			if i >= rays_amount / 2:
-				if interest[i - rays_amount / 2] < 0.5:
-					interest[i - rays_amount / 2] = 0.75
-			else:
-				if interest[i + rays_amount / 2] < 0.5:
-					interest[i + rays_amount / 2] = 0.75
-			#if danger[i] > 0.0:
-				#interest[i] = 0.5
-			#else:
-				#interest[i] = max(0.5, interest_value)
-			#interest[i] = interest_value
+			desired_direction = parent.global_position.direction_to(target_position)
+		if not target_reached:
+			for i in rays_amount:
+				var interest_value: float = ray_directions[i].dot(desired_direction.normalized())
+				interest_value = remap(interest_value, -1.0, 1.0, 0.0, 1.0)
+				interest[i] = interest_value#max(0.0, interest_value)
 
 
 func set_danger() -> void:
-	var space_state: PhysicsDirectSpaceState2D = get_viewport().world_2d.direct_space_state
+	#var space_state: PhysicsDirectSpaceState2D = get_viewport().world_2d.direct_space_state
 	for i in rays_amount:
 		if Utils.validate([parent, target]):
-			var query: PhysicsRayQueryParameters2D = PhysicsRayQueryParameters2D.create(parent.global_position,
-			parent.global_position + ray_directions[i] * look_ahead)
-			query.collide_with_areas = true
-			query.exclude = [parent.get_rid(), target.get_rid()]
-			#query.exclude = [parent.get_rid()]
-			var result: Dictionary = space_state.intersect_ray(query)
+			#var query: PhysicsRayQueryParameters2D = PhysicsRayQueryParameters2D.create(parent.global_position,
+			#parent.global_position + ray_directions[i] * look_ahead)
+			#query.collide_with_areas = true
+			#query.exclude = [parent.get_rid(), target.get_rid()]
+			var result: Dictionary = get_collisions(ray_directions[i])#space_state.intersect_ray(query)
 			if result.is_empty():
 				danger[i] = 0.0
+				#danger[i] = move_toward(danger[i], 0.0, interest[i])
 				#if interest[i] > 0.5:
 				#	danger[i] = 0.0#min(0.0, danger[i] - interest[i])
 			else:
-				danger[i] = 1.0#max(wdanger[i], 1.0)#1.0
+				#print(parent.global_position.distance_to(result["position"]))
+				var distance: float = parent.global_position.distance_to(result["position"])
+				var score: float = remap(distance, 0.0, look_ahead, 1.0, 0.0)
+				danger[i] = score
+				interest[i] -= score
+
+
+func get_collisions(direction: Vector2, exclude_target: bool = true) -> Dictionary:
+	var space_state: PhysicsDirectSpaceState2D = get_viewport().world_2d.direct_space_state
+	var query: PhysicsRayQueryParameters2D = PhysicsRayQueryParameters2D.create(parent.global_position,
+	parent.global_position + direction * look_ahead)
+	query.collide_with_areas = true
+	if exclude_target:
+		query.exclude = [parent.get_rid(), target.get_rid()]
+	else:
+		query.exclude = [parent.get_rid()]
+	var result: Dictionary = space_state.intersect_ray(query)
+	return result
 
 
 func get_movement_direction() -> Vector2:
 	if Utils.validate([target, parent]):
 		if Utils.validate([nav_agent]):
-			# Set the navigation agent's target position to the target node's position
+			# Update the navigation agent's target position to the target node's position
 			nav_agent.target_position = target.global_position
 		
-		#set_danger()
+		var result: Dictionary = get_collisions(parent.global_position.direction_to(target.global_position)*look_ahead, false)
+		
+		if not result.is_empty():
+			has_line_of_sight = result["collider"] == target
+			if has_line_of_sight:
+				last_known_position = target.global_position
+				target_position = target.global_position
+			else:
+				target_position = last_known_position
+		else:
+			has_line_of_sight = false
+		
 		set_interest()
 		set_danger()
-		var added_interest: float = 0.5
+		
 		for i in rays_amount:
-			if danger[i] > 0.0:
-				pass
-				#interest[i] = 0.0
-				#interest[-i] = 1.0
-				if i >= rays_amount / 2:
-					if danger[i - rays_amount / 2] == 0.0:
-						interest[i - rays_amount / 2] = added_interest
-				else:
-					if danger[i + rays_amount / 2] == 0.0:
-						interest[i + rays_amount / 2] = added_interest
-				if interest[i] > 0.0 and danger[i] > 0.0:
-					interest[i] = 0.0
 			debug_draw.interest[i] = interest[i]
 			debug_draw.danger[i] = danger[i]
-				#interest[i] = 0.0
-				#interest[i-1] = 1.0
-				#pass
 				
-		var direction: Vector2 = Vector2.ZERO
-		for i in rays_amount:
-			pass
-			direction += ray_directions[i] * interest[i]
-			direction -= ray_directions[i] * danger[i]
+		if not target_reached:
+			direction = Vector2.ZERO
+			for i in rays_amount:
+				direction += ray_directions[i] * interest[i]
+		
+		if parent.global_position.distance_to(target_position) < look_ahead / 2.0:
+			# We reached the player
+			if target_position == target.global_position:
+				direction = Vector2.ZERO
+			# We reached the last known player position
+			else:
+				direction = Vector2.ZERO
+			target_reached = true
+		else:
+			if has_line_of_sight:
+				target_reached = false
+			debug_draw.dir = direction
+		if target_reached:
+			direction = direction.move_toward(Vector2.ZERO, 0.5)
+			if direction.distance_to(Vector2.ZERO) < 0.3:
+				pass#direction = Vector2.ZERO
 		direction = direction.normalized()
-		if parent.global_position.distance_to(target.global_position) < look_ahead / 2.0:
-			pass#direction = Vector2.ZERO
 		set_direction_string(direction)
 		return direction
 	return Vector2.ZERO
 
 
 func get_attack() -> bool:
-	return false
+	#return false
 	if Utils.validate([target, parent]):
 		return parent.global_position.distance_to(target.global_position) < look_ahead / 2.0
 	return false
