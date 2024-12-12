2b29e4873fc80f9595dfccd68c57f61e96f353aa
inconsistentlyMediocre <carlospcastro.ac@gmail.com>
2024-12-12 15:31:56 -0300

    More work done on reflection component

--- components/reflection_component.gd
+++ components/reflection_component.gd
@@ -2,15 +2,13 @@ extends Node2D
 
 
 @export var reflecting_surface: Node2D
+@export var player: Player
 
-@onready var reflection: Sprite2D = $Reflection
-@onready var reflections: Node2D = $Reflections
-@onready var mask: PointLight2D = $Mask
+@onready var reflections: Node2D = $Mask/Reflections
+@onready var mask: Sprite2D = $Mask
+@onready var sub_viewport: SubViewport = $CanvasLayer/SubViewportContainer/SubViewport
+@onready var sub_viewport_container: SubViewportContainer = $CanvasLayer/SubViewportContainer
 
 
 func _ready() -> void:
-	print(get_tree().get_root().get_path())
-	#var viewport: ViewportTexture = ViewportTexture.new()
-	#viewport.viewport_path = get_tree().get_root().get_path()
-	#reflection.texture = viewport
-	
+	pass#sub_viewport.add_child(reflecting_surface.duplicate())
